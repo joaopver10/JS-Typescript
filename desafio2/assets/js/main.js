9c0c20e802d5e74e1dbc86e6982da9f50356c68c
João Pedro Vitorino <joaovitorino@unigranrio.br>
2022-09-21 17:32:30 -0300

    Aulas adicionadas

--- desafio2/assets/js/main.js
+++ desafio2/assets/js/main.js
@@ -1,64 +1,59 @@
- function criaCalculadora(){
-    return{
-        display: document.querySelector('.display'),
+ function Calculadora(){
 
-        inicia(){
-            this.cliqueBotoes();
-         },
-         
-        clearDisplay(){
-            this.display.value = '';
-        },
+    this.display = document.querySelector('.display');
 
-        apagaUm(){
-            this.display.value = this.display.value.slice(0, -1);
-        },
+    this.capturaCliques = () =>{
+        document.addEventListener('click', e =>{
+            const el = e.target
+            if (el.classList.contains('btn-num')){
+                this.addNumDisplay(el)
+            }
+            if (el.classList.contains('btn-clear')){
+                this.clearDisplay()
+            }
+            if (el.classList.contains('btn-del')){
+                this.apagaUm()
+            }
+            if (el.classList.contains('btn-eq')){
+                this.realizaConta()
+            }
+        })
+    }
+    this.addNumDisplay = el =>{
+        this.display.value += el.innerText
+    }
 
-        realizaConta(){
-            let conta = this.display.value;
+    this.clearDisplay = () =>{
+        this.display.value = '';
+    }
 
-            try{
-                conta = eval(conta);
+    this.apagaUm = () =>{
+        this.display.value = this.display.value.slice(0, -1);
+    }
 
-                if(!conta){
-                    alert('Conta inválida');
-                    return;
-                }
+    this.realizaConta = () =>{
+        let conta = this.display.value;
 
-                this.display.value = String(conta);
-            }catch(e){
+        try{
+            conta = eval(conta);
+
+            if(!conta){
                 alert('Conta inválida');
                 return;
             }
-        },
 
+            this.display.value = String(conta);
+        }catch(e){
+            alert('Conta inválida');
+            return;
+        }
+    }
 
-
-        cliqueBotoes(){
-            document.addEventListener('click', function(e){
-                const el = e.target
-                if(el.classList.contains('btn-num')){
-                    this.btnParaDisplay(el.innerText)
-                }
-                if(el.classList.contains('btn-clear')){
-                    this.clearDisplay()
-                }
-                if(el.classList.contains('btn-del')){
-                    this.apagaUm()
-                }
-                if(el.classList.contains('btn-eq')){
-                    this.realizaConta()
-                }
-            }.bind(this))
-        },
-
-        btnParaDisplay(valor){
-            this.display.value += valor
-        },
-
-    };
+    this.inicia = () =>{
+        this.capturaCliques()
+    }
  }
 
 
- const calculadora = criaCalculadora();
+ const calculadora = new Calculadora();
  calculadora.inicia();
